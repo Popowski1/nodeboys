fe0a72a8f8fcc4af712475d1aafda02042e2071b
Shane <shanepopowski@yahoo.com>
2023-03-28 17:38:23 -0500

    finished index.js, seems to work

---
+++ Develop/dist/new-README.md
@@ -0,0 +1,28 @@
+
+  # l
+  ![badge](https://img.shields.io/badge/license-mit-blue)
+  
+  ## Table-of-Contents
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  ## [Description](#table-of-contents)
+
+  l
+  l
+  l
+  ## [Installation](#table-of-contents)
+  undefined
+  ## [Usage](#table-of-contents)
+  l
+   * [license](#license)
+        [mit](https://choosealicense.com/licenses/mit)
+  ## [Tests](#table-of-contents)
+  l
+  ## [Questions](#table-of-contents)
+  Please contact me using the following links:
+  [GitHub](https://github.com/l)
+  [Email: undefined](mailto:undefined)
+
--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,151 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+const fs = require('fs');
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'title?',
+        validate: titleInput => {
+            if(titleInput) {
+                return true;
+            }else{console.log('enter title');
+        return false;
+    }
+            }
+        },
+        {
+            type: 'input',
+            name: 'githubUsername',
+            message: 'git username?',
+            validate: githubInput => {
+                if(githubInput) {
+                    return true;
+                }else{console.log('enter git username');
+            return false;
+        }
+                }
+            },
+            {
+                type: 'input',
+                name: 'what',
+                message: 'What is this project?',
+                validate: whatInput => {
+                    if(whatInput) {
+                        return true;
+                    }else{console.log('enter project');
+                return false;
+            }
+                    }
+                },
+                 {
+                type: 'input',
+                name: 'why',
+                message: 'Why did you make this project?',
+                validate: whyInput => {
+                    if(whyInput) {
+                        return true;
+                    }else{console.log('enter why you made this project');
+                return false;
+            }
+                    }
+                },
+                {
+                    type: 'input',
+                    name: 'how',
+                    message: 'How did you makethis project?',
+                    validate: howInput => {
+                        if(howInput) {
+                            return true;
+                        }else{console.log('enter how you made this project');
+                    return false;
+                }
+                        }
+                    },
+                    {
+                        type: 'input',
+                        name: 'install',
+                        message: 'Provide install instructions',
+                        validate: installInput => {
+                            if(installInput) {
+                                return true;
+                            }else{console.log('Provide install instructions');
+                        return false;
+                    }
+                            }
+                        },
+                        {
+                            type: 'input',
+                            name: 'usage',
+                            message: 'How to use.',
+                            validate: usageInput => {
+                                if(usageInput) {
+                                    return true;
+                                }else{console.log('How do I use?');
+                            return false;
+                        }
+                                }
+                            },
+                            {
+                                type: 'list',
+                                name: 'license',
+                                message: 'Which license will you use for your project?',
+                                choices: ['mit', 'no license']
+                            },
+                            {
+                                type: 'input',
+                                name: 'test',
+                                message: 'How to test',
+                                validate: testInput => {
+                                    if (testInput) {
+                                        return true;
+                                    }else {
+                                        console.log('How do I test?');
+                                return false;
+                                    }
+                                }
+                            }
+
+
+        
+    
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const writeFile = fileContent => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('./dist/new-README.md', fileContent, err =>{
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve({ok: true, message:'file created'
+        });
+        });
+    });
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+const init = () => {
+    return inquirer.prompt(questions)
+    .then(readmeData => {return readmeData;})
+}
 
 // Function call to initialize app
-init();
+init()
+.then(readmeData => {
+    console.log(readmeData);
+    return generateMarkdown(readmeData);
+})
+.then(pageMD => {
+    return writeFile(pageMD);
+})
+.then(writeFileResponse => {
+    console.log(writeFileResponse.message);
+})
+.catch(err =>{console.log(err);
+})
